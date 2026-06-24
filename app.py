ad09a7ba36594e1ba55c4b4fcff61b82b32f2d67
Damon-UnityGames <damon@powell-inc.com>
2026-06-24 22:26:19 +1000

    Added: More pages + training + testing added, so model basically exists and functions Issues: Overfitting, horrendous UI, results outputted via terminal instead of UI

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+import torchvision
+from PIL import Image
+from torchvision.transforms import v2
 
 class cnn(torch.nn.Module):
     def __init__(self):
@@ -30,8 +32,7 @@ class cnn(torch.nn.Module):
         self.flatten = nn.Flatten()
         self.fc6 = nn.Linear(256, 4096) #Change this for actual values
         self.fc7 = nn.Linear(4096, 4096)
-        self.fc8 = nn.Linear(4096, 1000)
-
+        self.fc8 = nn.Linear(4096, 5)
     def forward(self, x):
         x = self.convi1(x)
         x = self.relu1(x)
@@ -53,20 +54,78 @@ class cnn(torch.nn.Module):
         x = self.fc7(x)
         x = self.fc8(x)
         return x
+
+device = torch.device("xpu" if torch.xpu.is_available() else "cpu")
+
+def runTraining(model, transform, epochCount = 1):
+    model.train()
+    trainingSet = torchvision.datasets.ImageFolder("animals/train", transform)
+    trainingLoader = torch.utils.data.DataLoader(trainingSet, 32, True, num_workers=4, pin_memory=True)
+    lossFunc = torch.nn.CrossEntropyLoss()
+    optimiser = torch.optim.SGD(model.parameters(), 0.005, 0.9)
+    print(epochCount)
+    print(f'Training set has {len(trainingSet)} instances')
+    for i in range(epochCount):
+        lastLoss = 0.0
+        for j, data in enumerate(trainingLoader):
+            inputs, labels = data
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            optimiser.zero_grad()
+            outputs = model(inputs)
+            loss = lossFunc(outputs, labels)
+            loss.backward() # Runs backgrop for gradients
+            optimiser.step() #Applies gradients
+            lastLoss = loss.item()
+            if j % 100 == 0:
+                print(j)
+        print("Loss from epoch ", i, " = ", lastLoss)
+    torch.save(model.state_dict(), "cnn.pth")
+
+def runTest(model, image):
+    model.eval()
+    outputs = model(image)
+    print(outputs)
+
 app = Flask(__name__)
 
-model = cnn()
-model.eval()
-
-x = torch.randn(1, 3, 224, 224) #Image, randomn init rn
-out = model(x)
-print(torch.xpu.is_available()) #Need to add support for intel gpu
-print(torch.xpu.device_count())
-print(out.shape)
 @app.route("/")
 def home():
     return render_template("home.html")
 
+@app.route("/train", methods=['GET', 'POST'])
+def train():
+    if request.method == "POST":
+        action = request.form.get("action")
+        epochCount = int(request.form.get("epochCount"))
+        if action == "beginTrain":
+            runTraining(model, transform, epochCount)
+    return render_template("train.html")
+
+@app.route("/test", methods=['GET', 'POST'])
+def test():
+    if request.method == "POST":
+        testImage = request.files.get("testImage")
+        if testImage is None:
+            print("No test image")
+        else:
+            testImage = Image.open(testImage.stream).convert("RGB")
+            testImage = transform(testImage)
+            testImage = testImage.unsqueeze(0).to(device)
+            runTest(model, testImage)
+    return render_template("test.html")
 
 if __name__ == "__main__":
+    model = cnn()
+    model.load_state_dict(torch.load("cnn.pth", map_location="cpu"))
+    model = model.to(device)
+
+    transform = v2.Compose([
+        v2.Resize((224, 224)),
+        v2.ToImage(),
+        v2.ToDtype(torch.float32, scale=True)
+    ])
+
+    print(torch.xpu.is_available())  # Need to add support for intel gpu
+    print(torch.xpu.device_count())
     app.run(debug=True)
